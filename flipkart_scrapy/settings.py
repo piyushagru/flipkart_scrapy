c90f02a7c78f180f8ad6992b3a649b2cc406a234
Piyush <piyushnoida14@gmail.com>
2019-05-30 22:40:32 +0530

    scraping done right

--- flipkart_scrapy/settings.py
+++ flipkart_scrapy/settings.py
@@ -21,6 +21,8 @@ USER_AGENT = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
+PROXY_POOL_ENABLED = True
+
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
 
@@ -49,12 +51,15 @@ ROBOTSTXT_OBEY = True
 #SPIDER_MIDDLEWARES = {
 #    'flipkart_scrapy.middlewares.FlipkartScrapySpiderMiddleware': 543,
 #}
-
+# DOWNLOADER_MIDDLEWARES = {
+#     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+#     'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+# }
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'flipkart_scrapy.middlewares.FlipkartScrapyDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   'flipkart_scrapy.middlewares.FlipkartScrapyDownloaderMiddleware': 543,
+}
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
